28a317c6fefd9f89bb66ce8cbd3fb146e4282095
Gabriel Schreiner Bueno de Camargo <gsbcamargo@gmail.com>
2022-03-24 19:53:01 -0300

    creates setTotalAmount tests

--- src/test/entities/FinancingTests.java
+++ src/test/entities/FinancingTests.java
@@ -1,5 +1,6 @@
 package test.entities;
 
+import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
@@ -8,26 +9,53 @@ import org.junit.jupiter.api.Test;
 import entities.Financing;
 
 public class FinancingTests {
-	
+
 	@Test
 	public void constructorShouldCreateNewObjectWhenDataValid() {
 		Double totalAmount = 1000.0;
 		Double income = 5000.0;
 		Integer months = 24;
-		
+
 		Financing financing = new Financing(totalAmount, income, months);
-		
+
 		assertEquals(1000.0, financing.getTotalAmount());
 		assertEquals(5000.0, financing.getIncome());
 		assertEquals(24, financing.getMonths());
 	}
-	
+
 	@Test
 	public void constructorShouldThrowExceptionWhenDataInvalid() {
 		Double totalAmount = 5000.0;
 		Double income = 1000.0;
 		Integer months = 5;
-		
+
 		assertThrows(IllegalArgumentException.class, () -> new Financing(totalAmount, income, months));
+	}
+
+	@Test
+	public void totalAmountShouldUpdateWhenSetTotalAmountDataValid() {
+		Double totalAmount = 1000.0;
+		Double income = 5000.0;
+		Integer months = 24;
+		Double updatedTotalAmount = 1200.0;
+
+		Financing financing = new Financing(totalAmount, income, months);
+
+		financing.setTotalAmount(updatedTotalAmount);
+
+		assertTrue(financing.getTotalAmount() == updatedTotalAmount);
+	}
+
+	@Test
+	public void totalAmountShouldThrowExceptionWhenSetTotalAmountDataInvalid() {
+		Double totalAmount = 1000.0;
+		Double income = 5000.0;
+		Integer months = 24;
+		Double updatedTotalAmount = 120000.0;
+
+		assertThrows(IllegalArgumentException.class, () -> {
+			Financing financing = new Financing(totalAmount, income, months);
+			financing.setTotalAmount(updatedTotalAmount);
+		});
 	}
 }
